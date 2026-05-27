172d96d1776945c4bbb14cfaeee4346d3694a14a
ramoncordeiro <ramonestii@gmail.com>
2026-05-27 17:36:48 -0300

    preparing providers.tf for CI_CD

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,7 +7,12 @@ terraform {
   }
 }
 
-provider "kubernetes" {
-  config_path = "/etc/rancher/k3s/k3s.yaml"
+variable "kubeconfig_path" {
+  description = "path to kubeconfig file to connect to the Kubernetes cluster"
+  type        = string
+  default     = "/etc/rancher/k3s/k3s.yaml"
+}
 
+provider "kubernetes" {
+  config_path = pathexpand(var.kubeconfig_path)
 }
