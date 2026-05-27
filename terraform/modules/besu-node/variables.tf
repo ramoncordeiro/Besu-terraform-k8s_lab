f04a012fb4f13f14ddf1c69d00ef2ece0b690c3c
ramoncordeiro <ramonestii@gmail.com>
2026-05-27 18:50:00 -0300

    style: format terraform files. Workflow was breaking

--- terraform/modules/besu-node/variables.tf
+++ terraform/modules/besu-node/variables.tf
@@ -1,46 +1,46 @@
 variable "node_name" {
-    description = "Nome Unico para no (ps: validator01, boot,writer)"
-    type = string
+  description = "Nome Unico para no (ps: validator01, boot,writer)"
+  type        = string
 }
 
 variable "node_type" {
-    description = "Tipo de No (ps: validator, boot, writer, observer)"
-    type = string
-    default = "validator"
+  description = "Tipo de No (ps: validator, boot, writer, observer)"
+  type        = string
+  default     = "validator"
 }
 
 variable "genesis_configmap_name" {
-    description = "genesis.json"
-    type = string
+  description = "genesis.json"
+  type        = string
 }
 
 variable "static_nodes_configmap_name" {
-    description = "static-node.json file"
-    type = string
-    default = ""
+  description = "static-node.json file"
+  type        = string
+  default     = ""
 }
 
 variable "node_secret_name" {
-    description = "key"
-    type = string
+  description = "key"
+  type        = string
 }
 
-variable besu_image {
-    description = "Image Docker do Besu"
-    type = string
-    default = "hyperledger/besu:25.5.0"
+variable "besu_image" {
+  description = "Image Docker do Besu"
+  type        = string
+  default     = "hyperledger/besu:25.5.0"
 }
 
 variable "storage_size" {
-    description = "Tamanho do PVC para chain data"
-    type = string
-    default = "5Gi"
+  description = "Tamanho do PVC para chain data"
+  type        = string
+  default     = "5Gi"
 }
 
 variable "replicas" {
-    description = "Numero de replicas do sttefulset (deixei padrao 1)"
-    type = number
-    default = 1
+  description = "Numero de replicas do sttefulset (deixei padrao 1)"
+  type        = number
+  default     = 1
 }
 
 /*
